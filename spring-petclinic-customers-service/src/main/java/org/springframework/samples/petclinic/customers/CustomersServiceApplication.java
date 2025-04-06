68b3c42d6cbf7019750dbfc9f0b8151dc1b442b0
Nguyen Manh Phuong <manhphuong5514@gmail.com>
2025-04-07 02:15:04 +0700

    asdf

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
@@ -27,7 +27,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class CustomersServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Starting CustomersServiceApplication...");
+		System.out.println("Starting CustomersServiceApplicationADS...");
 		SpringApplication.run(CustomersServiceApplication.class, args);
 	}
 }
