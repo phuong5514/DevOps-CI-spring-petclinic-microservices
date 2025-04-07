b2ce996f75ec06a01b798698908467a3209d1ac0
Nguyen Manh Phuong <manhphuong5514@gmail.com>
2025-04-07 08:45:15 +0700

    h

--- spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/VisitsServiceApplication.java
+++ spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/VisitsServiceApplication.java
@@ -27,7 +27,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class VisitsServiceApplication {
 
     public static void main(String[] args) {
-        System.out.println("Starting Visits Service...");
+        System.out.println("Starting Visits Service..fff.");
         SpringApplication.run(VisitsServiceApplication.class, args);
     }
 }
